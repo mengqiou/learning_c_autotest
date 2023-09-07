8f87b855d9d67960a3195273bbe113ba3bb6b724
mengqiou <mengqiou92@gmail.com>
2023-09-07 13:47:38 +1000

    Add two sum files to be checked

---
+++ two_sum.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+#include "two_sum.h"
+
+
+int two_sum(int a, int b) {
+	return a + b;
+}
---
+++ two_sum.h
@@ -0,0 +1,7 @@
+#ifndef ADDITION_H
+#define ADDITION_H
+
+int two_sum(int a, int b);
+
+#endif /* ADDITION_H */
+
